309453fd2c1165a0cb9f450faaeda1655239d8b1
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-06-25 21:35:45 +0300

    +_

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const gallery = document.querySelector('.gallery')
+const boxes = document.querySelector('#boxes')
+const create = document.querySelector('button[data-action="render"]')
+const remove = document.querySelector('button[data-action="destroy"]')
+// const children = boxes.children;
 const scrollAmount = 5; 
 document.addEventListener('keydown', event => {
     event.preventDefault();
@@ -8,4 +12,23 @@ document.addEventListener('keydown', event => {
     if(event.code === 'KeyA') {
         gallery.scrollLeft -= scrollAmount
     }
+})
+
+function random() {
+    return Math.random() * 256;
+}
+
+// function createBoxes(amount) {
+    
+// }
+create.addEventListener('click', ()=> {
+    const div = document.createElement('div')
+    console.log(div);
+    div.classList.add('boxes__item')
+    div.style.backgroundColor = `rgb(${random()},${random()},${random()})`
+    div.style.height += '10px'
+    div.style.width += '10px'
+    div.textContent = 'ppp'
+    boxes.appendChild(div);
+    console.log(boxes);
 })
