c6375fed270276823c7844138818b146bc9324e4
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-06-30 16:26:03 +0300

    reafy

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const gallery = document.querySelector('.gallery')
 const boxes = document.querySelector('#boxes')
 const create = document.querySelector('button[data-action="render"]')
+const input = document.querySelector('input')
 const remove = document.querySelector('button[data-action="destroy"]')
+let div;
 // const children = boxes.children;
 const scrollAmount = 5;
 let elementSize = 30;
@@ -20,18 +22,22 @@ function random() {
     return Math.random() * 256;
 }
 
-// function createBoxes(amount) {
-    
-// }
+function createBoxes(amount) {
+    for (let i = 1; i <= amount; i++) {
+        div = document.createElement('div')
+        div.classList.add('boxes__item')
+        div.style.backgroundColor = `rgb(${random()},${random()},${random()})`
+        div.style.height = `${elementSize}px`;
+        div.style.width = `${elementSize}px`;
+        elementSize += 10;
+        div.textContent = ' '
+        boxes.appendChild(div);
+    }
+}
+create.addEventListener('click', () => createBoxes(input.value))
 
-create.addEventListener('click', ()=> {
-
-    const div = document.createElement('div')
-    div.classList.add('boxes__item')
-    div.style.backgroundColor = `rgb(${random()},${random()},${random()})`
-    div.style.height = `${elementSize}px`;
-    div.style.width = `${elementSize}px`;
-    elementSize += 10;
-    div.textContent = 'ppp'
-    boxes.appendChild(div);
-})
+function  destroyBoxes() {
+    boxes.innerHTML ='';
+    elementSize = 30;
+}
+remove.addEventListener('click', destroyBoxes)
